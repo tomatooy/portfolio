66ee38050999425fa6d9ef90720e2fb4bd825e7a
tomatooy <ouyangzongxin@gmail.com>
2023-11-03 02:12:50 -0400

    git link

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,7 +56,7 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/tomatooy">
             <Image src={GithubIcon} className="w-6" alt="Github Icon" />
           </Link>
           <Link href="linkedin.com">

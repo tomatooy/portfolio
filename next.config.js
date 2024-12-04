7de7ea7236a39afe24564e205782960b4f7288d8
tomatooy <ouyangzongxin@gmail.com>
2024-12-03 22:47:34 -0500

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
     reactStrictMode: true,
     env: {
         EMAIL_KEY: process.env.EMAIL_KEY,
-    }
+    },
+    webpack: (config) => {
+        config.cache = false;
+        return config;
+      },
 }
 
 module.exports = nextConfig
